9d52ba637ec0e0194444c39765a78edcb8d35e8c
Dhruv Dhar <dhruvdhar1@gmail.com>
2021-10-26 12:58:48 -0400

    added nav-bar, font awesome

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import NavBar from '../NavBar/NavBar';
+import './Layout.css'
+
+class Layout extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <div className="container-fluid">
+                <div className="row nav-bar">
+                    <NavBar />
+                </div>
+                <div className="row root-content">
+                    {/* <div className="col-1 side-bar">{this.props.left}</div>
+                    <div className="col-10 main-content"> {this.props.main}</div>
+                    <div className="col-1 right-side-bar">{this.props.right}</div> */}
+                </div>
+            </div>
+        )
+    }
+}
+export default Layout;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Layout from '../../components/Layout/Layout';
+import Button from '../../components/Button/Button';
+
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -6,10 +9,49 @@ class Home extends Component {
 
         }
     }
+
+    renderMainContent() {
+        return(
+            <div>
+                Hello, This is the home page.
+                <Button text={"Button 1"} onClick={e => this.buttonCLickHandler1(e)} />
+                <Button text={"Button 2"} onClick={e => this.buttonCLickHandler2(e)} />
+            </div>
+        )
+    }
+
+    renderLeftContent() {
+        return(
+            <div>
+                Left side.
+            </div>
+        )
+    }
+
+    renderRightContent() {
+        return(
+            <div>
+                Right Side.
+            </div>
+        )
+    }
+
+    buttonCLickHandler1(e) {
+        alert("Button 1 Clicked!");
+    }
+
+    buttonCLickHandler2(e) {
+        alert("Button 2 Clicked!");
+    }
+
     render() {
         return(
             <div>
-                This is Home...
+                <Layout
+                    left={this.renderLeftContent()} 
+                    main={this.renderMainContent()}
+                    right={this.renderRightContent()}
+                />
             </div>
         )
     }
