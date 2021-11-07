7f40b86ea3dceb2135a62c13c1d1893239cf1f9f
Dhruv Dhar <dhruvdhar1@gmail.com>
2021-11-06 22:18:33 -0400

    home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,16 +3,21 @@ import Layout from '../../components/Layout/Layout';
 import Button from '../../components/Button/Button';
 import Category from '../../components/Category/Category';
 import CommonCategory from "../../config/CommonCategories.json"
+import offer from "../../config/offer.json";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import HomeCarouselImages from '../../config/HomeCarouselImages.json';
 import "./Home.css";
+import Label from '../../components/Label/Label';
+import ItemsCarousel from 'react-items-carousel';
+import Product from '../../components/Product/Product';
 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            commonCategoryIndex: 0,
+            offerItemsIndex: 0
         }
     }
 
@@ -21,6 +26,9 @@ class Home extends Component {
             <div>
                 <div className="home-corousel-container">
                 <Carousel
+                    interval={4000}
+                    autoPlay
+                    infiniteLoop
                     showArrows={true}
                     onChange={()=>{}}
                     onClickItem={()=>{}}
@@ -37,16 +45,54 @@ class Home extends Component {
                 </Carousel>
                 </div>
                 <br/>
-                <div className="category-container">
-                    {
-                        CommonCategory.map((category, idx) => (
-                            <Category CategoryLabel={category.name} key={idx}/>  
-                        ))
-                    }
+                <Label text={"Your Favourite Categories"} customClass="grey-medium-bold-label" />
+                <div>
+                    {/* TODO: Add responsiveness */}
+                    <ItemsCarousel
+                            gutter={10}
+                            chevronWidth={60}
+                            numberOfCards={5}
+                            slidesToScroll={2}   
+                            outsideChevron={true}
+                            activeItemIndex={this.state.commonCategoryIndex}
+                            requestToChangeActive={value => this.setState({ commonCategoryIndex: value })}
+                            rightChevron={'>'}
+                            leftChevron={'<'}
+                    >
+                        {
+                            CommonCategory.map((category, idx) => (
+                                <Category CategoryLabel={category.name} key={idx}/>  
+                            ))
+                        }
+                        </ItemsCarousel>
                 </div>
-                Hello, This is the home page.
+                <br/>
+                <Label text={"Dont miss these offers"} customClass="grey-medium-bold-label" />
+                <ItemsCarousel
+                            gutter={10}
+                            chevronWidth={60}
+                            numberOfCards={5}
+                            slidesToScroll={2}   
+                            outsideChevron={true}
+                            activeItemIndex={this.state.offerItemsIndex}
+                            requestToChangeActive={value => this.setState({ offerItemsIndex: value })}
+                            rightChevron={'>'}
+                            leftChevron={'<'}
+                    >
+                        {
+                            offer.map(item => (
+                                <Product
+                                    itemName={item.itemName}
+                                    cost={item.cost}
+                                    originalPrice={item.originalPrice}
+                                    rating={item.rating}
+                                />
+                            ))
+                        }
+                </ItemsCarousel>
+                {/* Hello, This is the home page.
                 <Button text={"Button 1"} onClick={e => this.buttonCLickHandler1(e)} />
-                <Button text={"Button 2"} onClick={e => this.buttonCLickHandler2(e)} />
+                <Button text={"Button 2"} onClick={e => this.buttonCLickHandler2(e)} /> */}
             </div>
         )
     }
