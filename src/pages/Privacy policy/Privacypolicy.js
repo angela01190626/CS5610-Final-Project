b60a43c80d7dd0a2949ee55d818ce31a8e037553
Yangjiawen Xu <xuxx1262@umn.edu>
2021-12-15 01:00:09 +0800

    privacy policy edited, feel free to add more stuff

--- src/pages/Privacy policy/Privacypolicy.js
+++ src/pages/Privacy policy/Privacypolicy.js
@@ -13,7 +13,7 @@ class Privacypolicy extends Component {
                     <h1>Privacy Policy</h1>
                     <h2>Introduction</h2>
                     <p>
-                        Thank you for using the Online Shopping Website! We are committed to providing you the best
+                        Thank you for using ShopEazy, the Online Shopping Website! We are committed to providing you the best
                         online
                         shopping and delivery experience possible. This Privacy Policy explains what information we
                         collect,
@@ -27,48 +27,55 @@ class Privacypolicy extends Component {
                         When you are using this online shopping website, you may need to provide some information about
                         yourself.
                         The information we collect from you includes things such as: <br/>
-                        • Name<br/>
                         • Email address<br/>
+                        • Password (It will be encrypted after you create your account and save the new password.) <br/>
+                        • First and Last Name<br/>
                         • Phone number<br/>
                         • Date of birth<br/>
                         • Postal address<br/>
                         • Credit/debit card information<br/>
                         In some cases, you need to edit your personal information, you can go to your profile to
-                        update/delete your address, phone number, email address, credit/debit information.
+                        update your password, first and last name, date of birth, delivery address, phone number, email address,
+                        credit/debit information. <br/>
+
+                        In some circumstances, you may choose to be our paid member and follow/unfollow the other uses in our website.
+
+
                     </p>
                     <h3>Information automatically collected</h3>
                     <p>We collect information about your interactions with us including, for example:<br/>
-                        • The type of device or browser you’re using<br/>
-                        • Your IP address<br/>
-                        • Your browsing behavior while on our website
+                        • We use a third-party api to collect information about the use of our websites. This api will show how many requests
+                        you have made when you use our website.
                     </p>
                     <h2>How we use your information</h2>
                     <p>
                         We will use your information to meet your expectations and give you a great experience with our
-                        online shopping website.
-                        For example, when your place an order, we will send your order to a retailer store, and then
-                        provide your information to
-                        delivery team and call/text with any updates or issues. We will charge your for the purchase and
-                        delivery costs.
+                        online shopping website. For example,
+                        • To create and maintain your account<br/>
+                        • To determine the delivery fees<br/>
+                        • To analyze your shopping behaviors<br/>
+                        • To update our technical functionality<br/>
+                        • To analyze different age groups' preferences<br/>
+
                     </p>
                     <h2>What we share</h2>
                     <p>
                         Our online shopping website does not exchange your personal information for monetary
                         consideration. For example,
                         we may share your information with third parties to fulfill orders, deliver packages, process
-                        credit/debit cards,
-                        provide customer care, conduct research and analysis, detect fraud and other illegal activity.
+                        credit/debit cards, provide customer care, conduct research and analysis,
+                        detect fraud and other illegal activity.
                     </p>
                     <h2>Security</h2>
                     <p>
-                        We use a variety of security measures designed, under a risk-based approach, to ensure the
-                        confidentiality of your information.
-                        However, as set forth in the applicable terms of use for each digital property, we cannot and do
-                        not guarantee the security of information transmitted to us.
+                        We recognize the importance of storing the customers' personal information.
+                        We apply a technique to protect your password.
+                        For example, when you create an account save your new password,
+                        your password will be encrypted.
                     </p>
                     <p>
                         You also can do your part to help protect the information you provide to us. Create a strong
-                        password. Use that password only on our digital properties.
+                        password. Use that password only on our website.
                         And do not share your password with anyone — especially anyone claiming to work with us. We
                         would never contact you and ask for your password.
                     </p>
